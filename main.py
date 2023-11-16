dd72e98169af8c658b170b0ea0457ed77c0608c2
lcard <louis.cardoso@efrei.net>
2023-11-16 11:35:47 +0100

    Commit 2

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello Louis")
+from fonction import *
+
+#Appel de la fonction
+directory = "./speeches/"
+files_names = list_of_files(directory, "txt")
+print(files_names)
+
+nom_fichier = directory + files_names[0]
+
+with open(nom_fichier, "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        print(line)
